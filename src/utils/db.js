cae4c43f754ffa09654d9f405678203cb5b13c12
cadbee <aschiam.net@gmail.com>
2023-07-02 20:45:11 +0300

    Fix live folder creating

--- src/utils/db.js
+++ src/utils/db.js
@@ -6,9 +6,15 @@ import lodash from 'lodash';
 import layers from "./data/layers.js";
 import signs from "./data/signs.js";
 import troops from "./data/troops.js";
+import * as fs from "fs";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+
+const liveFolderPath = path.join(__dirname, '../../live');
+if (!fs.existsSync(liveFolderPath)) {
+    fs.mkdirSync(liveFolderPath);
+}
 
 const adapter = new JSONFileSync(path.join(__dirname, '../../live/db.json'));
 
